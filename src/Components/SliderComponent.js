91c55e3d84ce873bd65e503fe4f7f4ffc6497387
bhavay puri <bhavaypuri15@gmail.com>
2019-04-10 13:29:23 +0530

    integrated APIs

--- src/Components/SliderComponent.js
+++ src/Components/SliderComponent.js
@@ -12,10 +12,13 @@ const SliderComponent = props => {
                 <img src="https://s3-ap-southeast-1.amazonaws.com/he-public-data/Left%20Control577660a.png" />
             </a>
             <ul>
-                <li><img src="http://mgzavrebi.ge/site/local_sources/images/room_photo/room_photo_1_03a21d6e6d84361de45d6c83a51d77d0.jpg"/></li>
-                <li style={{background: '#aaa'}}><img src="https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"/></li>
-                <li><img src="https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"/></li>
-                <li style={{background: '#aaa'}}>SLIDE 4</li>
+                {
+                    props.images.map(image => (
+                        <li key={image.data.id}>
+                            <img src={image.data.url} />
+                        </li>
+                    ))
+                }
             </ul>  
         </div>
         </Fragment>
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
+const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const dotenv = require('dotenv');
 
+const env = dotenv.config().parsed;
+const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+}, {})
 module.exports = {
     entry: './src/index.js',
     output: {
@@ -33,6 +40,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html'
-        })
+        }),
+        new webpack.DefinePlugin(envKeys)
     ]
 }
